030aa08203d33fcee61a6d452c151bac47a748d2
sginko <142085017+sginko@users.noreply.github.com>
2024-09-24 11:16:54 +0200

    add

--- src/main/java/com/example/ginko/kafkaconsumerfinancespring/service/KafkaConsumerFinanceService.java
+++ src/main/java/com/example/ginko/kafkaconsumerfinancespring/service/KafkaConsumerFinanceService.java
@@ -2,27 +2,42 @@ package com.example.ginko.kafkaconsumerfinancespring.service;
 
 import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+
 @AllArgsConstructor
 @Service
 public class KafkaConsumerFinanceService {
+    //    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerFinanceService.class);
     private final KafkaTemplate<String, String> kafkaTemplate;
+
 
     @KafkaListener(topics = "orders", groupId = "finance-group")
     public void consume(ConsumerRecord<String, String> record) {
-        System.out.println("Finance consumed message: " + record.value());
+        String consumedMessage = record.value();
+        System.out.println("Finance consumed message: " + consumedMessage);
 
-        String confirmationMessage = "Finance confirmation: " + record.value();
-        sendConfirmation(confirmationMessage);
+        String replyMessage = "Processed: " + consumedMessage;
+        kafkaTemplate.send("orders-confirmations", replyMessage);
+        System.out.println("Finance sent confirmation 'my-reply-topic': " + replyMessage);
     }
 
-    private void sendConfirmation(String message) {
-        ProducerRecord<String, String> record = new ProducerRecord<>("orders-confirmation", message);
-        kafkaTemplate.send(record);
-        System.out.println("Finance sent confirmation: " + message);
-    }
+//    @KafkaListener(topics = "orders", groupId = "finance-group")
+//    public void consume(ConsumerRecord<String, String> record) {
+//        System.out.println("Finance consumed message: " + record.value());
+//        logger.info("Consumed message: {}", record.value());
+//
+//        String confirmationMessage = "Finance confirmation: " + record.value();
+//        sendConfirmation(confirmationMessage);
+//
+//    }
+//
+//    private void sendConfirmation(String message) {
+//        ProducerRecord<String, String> record = new ProducerRecord<>("orders-confirmation", message);
+//        kafkaTemplate.send(record);
+//        kafkaTemplate.send("orders-confirmations", message);
+//        System.out.println("Finance sent confirmation: " + message);
+//    }
 }
--- src/main/java/com/example/ginko/kafkaconsumerwarehousespring/service/KafkaConsumerWarehouseService.java
+++ src/main/java/com/example/ginko/kafkaconsumerwarehousespring/service/KafkaConsumerWarehouseService.java
@@ -2,7 +2,6 @@ package com.example.ginko.kafkaconsumerwarehousespring.service;
 
 import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -12,17 +11,27 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumerWarehouseService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @KafkaListener(topics = "orders", groupId = "warehouse-group")
+    @KafkaListener(topics = "orders", groupId = "finance-group")
     public void consume(ConsumerRecord<String, String> record) {
-        System.out.println("Warehouse consumed message: " + record.value());
+        String consumedMessage = record.value();
+        System.out.println("Warehouse consumed message: " + consumedMessage);
 
-        String confirmationMessage = "Warehouse confirmation: " + record.value();
-        sendConfirmation(confirmationMessage);
+        String replyMessage = "Processed: " + consumedMessage;
+        kafkaTemplate.send("orders-confirmations", replyMessage);
+        System.out.println("Warehouse sent confirmation 'orders': " + replyMessage);
     }
 
-    private void sendConfirmation(String message) {
-        ProducerRecord<String, String> record = new ProducerRecord<>("orders-confirmation", message);
-        kafkaTemplate.send(record);
-        System.out.println("Warehouse sent confirmation: " + message);
-    }
+//    @KafkaListener(topics = "orders", groupId = "warehouse-group")
+//    public void consume(ConsumerRecord<String, String> record) {
+//        System.out.println("Warehouse consumed message: " + record.value());
+//
+//        String confirmationMessage = "Warehouse confirmation: " + record.value();
+//        sendConfirmation(confirmationMessage);
+//    }
+//
+//    private void sendConfirmation(String message) {
+//        ProducerRecord<String, String> record = new ProducerRecord<>("orders-confirmation", message);
+//        kafkaTemplate.send(record);
+//        System.out.println("Warehouse sent confirmation: " + message);
+//    }
 }
